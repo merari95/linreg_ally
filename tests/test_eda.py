14d212cc87bce07653f35f5ed077ab698d078eac
Paramveer Singh <paramveer.singh238@outlook.com>
2025-01-15 10:47:42 -0800

    Finish expected case tests

--- tests/test_eda.py
+++ tests/test_eda.py
@@ -3,5 +3,48 @@
 # 15 January 2025
 
 import pytest
+import altair as alt
+from vega_datasets import data
 from linreg_ally.eda import eda_summary
 
+@pytest.fixture
+def iris_data():
+    return data.iris()
+
+def test_good_data_no_color(iris_data):
+    # Store result
+    plot = eda_summary(iris_data)
+
+    # Assert output type
+    assert isinstance(plot, alt.ConcatChart), 'Function does not return correct type!'
+    
+    # Assert x's are correct
+    feature_names = iris_data.columns.tolist()
+    feature_names = feature_names.remove('species')
+    
+    feats_used = []
+    for i in range(len(plot.concat)):
+        feats_used.append(plot.concat[i].layer[0].encoding.x.shorthand)
+
+    assert set(feature_names) == set(feats_used), 'All x variables are not used!'
+
+def test_good_data_color(iris_data):
+    # Store result
+    plot = eda_summary(iris_data, color='species')
+
+    # Assert output type
+    assert isinstance(plot, alt.ConcatChart), 'Function does not return correct type!'
+
+    # Assert x's are correct
+    feature_names = iris_data.columns.tolist()
+    feature_names = feature_names.remove('species')
+
+    feats_used = []
+    for i in range(len(plot.concat)):
+        feats_used.append(plot.concat[i].layer[0].encoding.x.shorthand)
+
+    assert set(feature_names) == set(
+        feats_used), 'All x variables are not used!'
+
+    # Assert color is `species`
+    assert plot.concat[0].layer[0].color.shorthand == 'species', 'The wrong column is used for coloring!'
