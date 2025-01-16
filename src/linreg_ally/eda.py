e313db216ca0a722d128ab63addc0e078203c78d
Paramveer Singh <paramveer.singh238@outlook.com>
2025-01-16 11:02:56 -0800

    Implement column dtype conversion to handle edge cases

--- src/linreg_ally/eda.py
+++ src/linreg_ally/eda.py
@@ -29,8 +29,6 @@ def eda_summary(train_df: pd.DataFrame, color: str = None) -> alt.ConcatChart:
         When `color` is not in the `train_df` columns
     TypeError
         When either `train_df` or `color` is not the expected type
-    ValueError
-        When column conversion dtype in `train_df` fails
 
     Examples
     --------
@@ -43,9 +41,15 @@ def eda_summary(train_df: pd.DataFrame, color: str = None) -> alt.ConcatChart:
         raise TypeError(f'train_df needs to be a Pandas DataFrame and not {type(train_df)}')
     if not isinstance(color, str) and color is not None:
         raise TypeError(f'color needs to be a string or None and not {type(color)}')
-    
+
     # Check if `color` is a column name in `train_df`
     if color is not None and color not in train_df.columns.tolist():
         raise KeyError(f'There is no {color} column in train_df')
+    
+    # Do column dtype conversion
+    plot_df = train_df.copy()
 
-    return aly.dist(train_df, color=color)
+    if color is not None:
+        plot_df[color] = plot_df[color].astype('category')
+
+    return aly.dist(plot_df, color=color)
