a43534990d552a2f7f91b03db3f86c62f9b5b433
Paramveer Singh <paramveer.singh238@outlook.com>
2025-01-15 19:01:47 -0800

    Update docstring and add basic functionality

--- src/linreg_ally/eda.py
+++ src/linreg_ally/eda.py
@@ -23,10 +23,19 @@ def eda_summary(train_df: pd.DataFrame, color: str = None) -> alt.ConcatChart:
     alt.ConcatChart
         A chart that shows the distributions of various input features
 
+    Raises
+    ------
+    KeyError
+        When `color` is not in the `train_df` columns
+    TypeError
+        When either `train_df` or `color` is not the expected type
+    ValueError
+        When column conversion dtype in `train_df` fails
+
     Examples
     --------
     >>> from linreg_ally.eda import eda_summary
     >>> summary_chart = eda_summary(train_df, color='y')
     >>> summary_chart.show()        
     """
-    pass
+    return aly.dist(train_df, color=color)
